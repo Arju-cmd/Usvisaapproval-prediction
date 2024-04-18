6f957f03b8e3a44d932ab76c66fc8441253e8465
Arju-cmd <arjungkumar333@gmail.ocm>
2024-04-18 11:45:36 +0530

    setuppy

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Arjun",
+    author_email="arjungkumar333@gmail.com",
+    packages=find_packages()
+)
